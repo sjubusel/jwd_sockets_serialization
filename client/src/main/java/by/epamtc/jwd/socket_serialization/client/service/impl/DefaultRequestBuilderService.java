56fce54a9375bcdf179efc660958e0661944951c
Siarhei Busiel <sju.busel@gmail.com>
2020-08-17 14:50:43 +0300

    Add "addFileName" method in DefaultRequestBuilderService

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/impl/DefaultRequestBuilderService.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/impl/DefaultRequestBuilderService.java
@@ -1,11 +1,13 @@
 package by.epamtc.jwd.socket_serialization.client.service.impl;
 
 import by.epamtc.jwd.socket_serialization.client.service.RequestBuilderService;
+import by.epamtc.jwd.socket_serialization.model.Request;
 
 public class DefaultRequestBuilderService implements RequestBuilderService {
+    private Request request = new Request();
 
     @Override
     public void addFileName(String userInput) {
-
+        request.setFileName(userInput);
     }
 }
