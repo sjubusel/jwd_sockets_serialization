c0dfbc96a25b51fb9a8494497de47a8b61bcfcc4
Siarhei Busiel <sju.busel@gmail.com>
2020-08-17 16:17:37 +0300

    Add operation-connected functionality to UserCommunicator

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/UserCommunicator.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/UserCommunicator.java
@@ -1,6 +1,14 @@
 package by.epamtc.jwd.socket_serialization.client.view;
 
+import by.epamtc.jwd.socket_serialization.model.RequestOperation;
+import by.epamtc.jwd.socket_serialization.model.RequestOperationEntry;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserCommunicator {
+    private Pattern ordinalOperationNumber = Pattern.compile("\\d");
+
     public void printDialogHeader() {
         System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
         System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
@@ -43,7 +51,7 @@ public class UserCommunicator {
         System.out.println("Если да, то введите символ \"Y\"");
     }
 
-    public boolean isFileNameCorrect(String userInput) {
+    public boolean isUserInputCorrect(String userInput) {
         return userInput.equals("Y");
     }
 
@@ -53,9 +61,48 @@ public class UserCommunicator {
 
     public void askUserAboutOperation() {
         System.out.println("Введите операцию с текстом.");
+        for (RequestOperation value : RequestOperation.values()) {
+            System.out.printf("Введите \"%s\" для выбора операции:\n" +
+                    "\"%s\"\n", value.getOrdinalNumber(), value.getDescription());
+        }
     }
 
-    public void informUserAboutChosenOperation(String userInput) {
-        System.out.printf("Вы выбрали операцию \"%s\"%n", userInput);
+    public RequestOperationEntry receiveChosenOperation(String userInput) {
+        userInput = userInput.replaceAll("\\s+", " ");
+        int ordinalNumberOfChosenOperation = -1;
+        RequestOperation chosenOperation = null;
+        String chosenOperationValue = "";
+        int paramStartIndex = -1;
+
+        Matcher matcher = ordinalOperationNumber.matcher(userInput);
+        if (matcher.find()) {
+            String operationNumber = matcher.group();
+            paramStartIndex = matcher.end() + 1;
+            ordinalNumberOfChosenOperation = Integer.parseInt(matcher.group());
+        }
+        if ((paramStartIndex > 0) && (paramStartIndex < userInput.length())) {
+            chosenOperationValue = userInput.substring(paramStartIndex);
+        }
+        for (RequestOperation operation : RequestOperation.values()) {
+            if (operation.getOrdinalNumber() == ordinalNumberOfChosenOperation) {
+                chosenOperation = operation;
+            }
+        }
+        return new RequestOperationEntry(chosenOperation, chosenOperationValue);
+    }
+
+    public void informUserAboutChosenOperation(RequestOperationEntry operation) {
+        System.out.printf("Вы выбрали операцию:\n%s\n",
+                operation.getOperationKey().getDescription());
+    }
+
+
+    public void askUserIsOperationCorrect() {
+        System.out.println("Вы ввели корректную операцию?");
+        System.out.println("Если да, то введите символ \"Y\"");
+    }
+
+    public void informUserOperationIsIncorrect() {
+        System.out.println("Вы не потдвердили операцию с текстом.");
     }
 }
