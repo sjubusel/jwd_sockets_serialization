812371a0f67994ad7685393a01945a956459afb3
Siarhei Busiel <sju.busel@gmail.com>
2020-08-19 20:09:39 +0300

    Add a ResponsePrinter stub

---
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/ResponsePrinter.java
@@ -0,0 +1,9 @@
+package by.epamtc.jwd.socket_serialization.client.view;
+
+import by.epamtc.jwd.socket_serialization.model.response.Text;
+
+public class ResponsePrinter {
+    public void print(Text responseText) {
+
+    }
+}
