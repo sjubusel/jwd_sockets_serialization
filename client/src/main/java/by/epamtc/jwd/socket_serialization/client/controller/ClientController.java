349e9fff36031b6f6754b79b3d1b2aca9c4b648e
Siarhei Busiel <sju.busel@gmail.com>
2020-08-19 16:17:26 +0300

    Edit intermediate changes to ClientController.java

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/controller/ClientController.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/controller/ClientController.java
@@ -3,23 +3,30 @@ package by.epamtc.jwd.socket_serialization.client.controller;
 import by.epamtc.jwd.socket_serialization.client.service.RequestBuilderService;
 import by.epamtc.jwd.socket_serialization.client.service.ServiceFactory;
 import by.epamtc.jwd.socket_serialization.client.view.UserCommunicator;
+import by.epamtc.jwd.socket_serialization.model.Request;
 import by.epamtc.jwd.socket_serialization.model.RequestOperationEntry;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.Socket;
 
 public class ClientController {
     private static UserCommunicator userCommunicator = new UserCommunicator();
+    private static ServiceFactory serviceFactory = ServiceFactory.getInstance();
+    private static RequestBuilderService requestBuilderService
+            = serviceFactory.getRequestBuilderService();
 
     public static void main(String[] args) {
-        ServiceFactory serviceFactory = ServiceFactory.getInstance();
-        RequestBuilderService requestBuilderService = serviceFactory.getRequestBuilderService();
-
         String userInput;
+        Socket clientSocket;
+        ObjectOutputStream oStream;
+        ObjectInputStream iStream;
 
         try (InputStreamReader isr = new InputStreamReader(System.in);
              BufferedReader reader = new BufferedReader(isr)) {
+            clientSocket = new Socket("localhost", 443);
+            oStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            iStream = new ObjectInputStream(clientSocket.getInputStream());
+
             userCommunicator.printDialogHeader();
 
             while (true) {
@@ -34,27 +41,46 @@ public class ClientController {
                 }
                 requestBuilderService.addFileName(userInput);
 
-                userCommunicator.askUserAboutOperation();
-                userInput = reader.readLine();
-                RequestOperationEntry operationEntry = userCommunicator
-                        .receiveChosenOperation(userInput.trim());
-                userCommunicator.informUserAboutChosenOperation(operationEntry);
-                userCommunicator.askUserIsOperationCorrect();
-                userInput = reader.readLine();
-                if (!userCommunicator.isUserInputCorrect(userInput)) {
-                    userCommunicator.informUserOperationIsIncorrect();
-                    continue;
-                }
-                requestBuilderService.addOperation(operationEntry);
+                while (true) {
+                    userCommunicator.askUserAboutOperation();
+                    userInput = reader.readLine();
+                    RequestOperationEntry operationEntry = userCommunicator
+                            .receiveChosenOperation(userInput.trim());
+                    userCommunicator.informUserAboutChosenOperation(operationEntry);
+                    userCommunicator.askUserIsOperationCorrect();
+                    userInput = reader.readLine();
+                    if (!userCommunicator.isUserInputCorrect(userInput)) {
+                        userCommunicator.informUserOperationIsIncorrect();
+                        continue;
+                    }
+                    requestBuilderService.addOperation(operationEntry);
 
-                userCommunicator.printDialogFooter();
+                    userCommunicator.askUserIsAnotherOperationNeeded();
+                    userInput = reader.readLine();
+                    if (!userCommunicator.isUserInputCorrect(userInput)) {
+                        break;
+                    }
+                }
+
+                userCommunicator.informRequestIsComplete();
+                Request request = requestBuilderService.buildRequest();
+                oStream.writeObject(request);
+                System.out.println("Запрос направлен на сервер...");
+
+                Text responseText = ((Text) iStream.readObject());
+
 
                 if (true) {
                     break;
                 }
+
             }
 
+            userCommunicator.printDialogFooter();
         } catch (IOException e) {
+            //TODO add exception handling
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             //TODO add exception handling
             e.printStackTrace();
         }
