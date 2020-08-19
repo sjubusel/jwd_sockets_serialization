5033688ffee2659bad63936f19a90d404691550d
Siarhei Busiel <sju.busel@gmail.com>
2020-08-20 01:57:04 +0300

    Add Server Controller

--- server/src/main/java/by/epamtc/jwd/socket_serialization/server/controller/ServerController.java
+++ server/src/main/java/by/epamtc/jwd/socket_serialization/server/controller/ServerController.java
@@ -1,14 +1,37 @@
 package by.epamtc.jwd.socket_serialization.server.controller;
 
-import by.epamtc.jwd.socket_serialization.server.service.ServerSocketService;
-import by.epamtc.jwd.socket_serialization.server.service.ServiceFactory;
+
+import by.epamtc.jwd.socket_serialization.server.controller.exception.ControllerException;
+import by.epamtc.jwd.socket_serialization.server.controller.util.MultithreadedServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServerController {
-    private static ServiceFactory serviceFactory = ServiceFactory.getInstance();
-    private static ServerSocketService serverSocketService
-            = serviceFactory.getServerSocketService();
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerController.class);
 
     public static void main(String[] args) {
+        int serverPort = 443;
+        int maxNumberOfClients = 10;
+        MultithreadedServer server = new MultithreadedServer(serverPort,
+                maxNumberOfClients);
 
+        try {
+            server.startServer();
+        } catch (ControllerException e) {
+            LOGGER.error("ERROR WHILE STARTING SERVER", e);
+        }
+
+
+        try {
+            Thread.sleep(60 * 1000);
+        } catch (InterruptedException e) {
+            LOGGER.error("WORK OF SERVER CONTROLLER IS INTERRUPTED.");
+        }
+
+        try {
+            server.stopServer();
+        } catch (ControllerException e) {
+            LOGGER.error("ERROR WHILE STOPPING SERVER", e);
+        }
     }
 }
