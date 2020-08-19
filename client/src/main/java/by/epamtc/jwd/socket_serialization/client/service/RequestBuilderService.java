a70bfba91c74095190f63fac02619ebf99592cdc
Siarhei Busiel <sju.busel@gmail.com>
2020-08-19 20:53:44 +0300

    Add a method "addText" to RequestBuilderService and its implementation (DefaultRequestBuilderService)

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/RequestBuilderService.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/RequestBuilderService.java
@@ -2,11 +2,14 @@ package by.epamtc.jwd.socket_serialization.client.service;
 
 import by.epamtc.jwd.socket_serialization.model.request.Request;
 import by.epamtc.jwd.socket_serialization.model.request.RequestOperationEntry;
+import by.epamtc.jwd.socket_serialization.model.response.Text;
 
 public interface RequestBuilderService {
     void addFileName(String userInput);
 
     void addOperation(RequestOperationEntry operation);
+
+    void addText(Text text);
 
     Request buildRequest();
 }
--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/impl/DefaultRequestBuilderService.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/impl/DefaultRequestBuilderService.java
@@ -3,6 +3,7 @@ package by.epamtc.jwd.socket_serialization.client.service.impl;
 import by.epamtc.jwd.socket_serialization.client.service.RequestBuilderService;
 import by.epamtc.jwd.socket_serialization.model.request.Request;
 import by.epamtc.jwd.socket_serialization.model.request.RequestOperationEntry;
+import by.epamtc.jwd.socket_serialization.model.response.Text;
 
 public class DefaultRequestBuilderService implements RequestBuilderService {
     private Request request = new Request();
@@ -15,6 +16,11 @@ public class DefaultRequestBuilderService implements RequestBuilderService {
     @Override
     public void addOperation(RequestOperationEntry operation) {
         request.addOperation(operation);
+    }
+
+    @Override
+    public void addText(Text text) {
+        request.setText(text);
     }
 
     @Override
