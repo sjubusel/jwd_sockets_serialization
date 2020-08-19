7c8b4771f1b9a114d2d04de0f4bd94d1717a12fe
Siarhei Busiel <sju.busel@gmail.com>
2020-08-19 20:06:43 +0300

    Add new service classes (exception, clientSocketService)

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/ServiceFactory.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/ServiceFactory.java
@@ -1,11 +1,12 @@
 package by.epamtc.jwd.socket_serialization.client.service;
 
+import by.epamtc.jwd.socket_serialization.client.service.impl.DefaultClientSocketService;
 import by.epamtc.jwd.socket_serialization.client.service.impl.DefaultRequestBuilderService;
 
 public class ServiceFactory {
     private static final ServiceFactory instance = new ServiceFactory();
-
     private final RequestBuilderService requestBuilderService = new DefaultRequestBuilderService();
+    private final ClientSocketService clientSocketService = new DefaultClientSocketService();
 
     private ServiceFactory() {
     }
@@ -16,5 +17,9 @@ public class ServiceFactory {
 
     public RequestBuilderService getRequestBuilderService() {
         return requestBuilderService;
+    }
+
+    public ClientSocketService getClientSocketService() {
+        return clientSocketService;
     }
 }
---
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/service/impl/DefaultClientSocketService.java
@@ -0,0 +1,56 @@
+package by.epamtc.jwd.socket_serialization.client.service.impl;
+
+import by.epamtc.jwd.socket_serialization.client.service.ClientSocketService;
+import by.epamtc.jwd.socket_serialization.client.service.exception.ServiceException;
+import by.epamtc.jwd.socket_serialization.model.response.Text;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class DefaultClientSocketService implements ClientSocketService {
+    private Socket clientSocket;
+    private ObjectOutputStream oStream;
+    private ObjectInputStream iStream;
+
+    @Override
+    public void startConnection(String address, int port) throws ServiceException {
+        try {
+            clientSocket = new Socket("localhost", 443);
+            oStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            iStream = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            throw new ServiceException("START SERVER CONNECTION ERROR");
+        }
+    }
+
+    @Override
+    public void sendObject(Object objectToSend) throws ServiceException {
+        try {
+            oStream.writeObject(objectToSend);
+        } catch (IOException e) {
+            throw new ServiceException("WRITING TO SERVER ERROR");
+        }
+    }
+
+    @Override
+    public Object receiveObject() throws ServiceException {
+        try {
+            return iStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new ServiceException("READING FROM SERVER ERROR");
+        }
+    }
+
+    @Override
+    public void stopConnection() throws ServiceException {
+        try {
+            iStream.close();
+            oStream.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            throw new ServiceException("STOPPING SERVER CONNECTION ERROR");
+        }
+    }
+}
