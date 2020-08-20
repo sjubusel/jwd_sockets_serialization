025e3ac158fbcf1023a42b94d8fedb3a07ae3e2c
Siarhei Busiel <sju.busel@gmail.com>
2020-08-20 11:38:09 +0300

    Add a "print" method of ResponsePrinter

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/ResponsePrinter.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/ResponsePrinter.java
@@ -4,6 +4,9 @@ import by.epamtc.jwd.socket_serialization.model.response.Text;
 
 public class ResponsePrinter {
     public void print(Text responseText) {
-
+        StringBuilder builder = new StringBuilder();
+        responseText.fillWithContents(builder);
+        System.out.println(new String(builder));
+        builder.delete(0, builder.length());
     }
 }
