4b52a6dd1d2a33dd9be34cabd9f589ecb085a564
Siarhei Busiel <sju.busel@gmail.com>
2020-08-20 21:23:22 +0300

    Clean up ResponsePrinter

--- client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/ResponsePrinter.java
+++ client/src/main/java/by/epamtc/jwd/socket_serialization/client/view/ResponsePrinter.java
@@ -5,7 +5,11 @@ import by.epamtc.jwd.socket_serialization.model.response.Text;
 public class ResponsePrinter {
     public void print(Text responseText) {
         StringBuilder builder = new StringBuilder();
+        builder.append("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" +
+                "▓▓▓▓▓▓▓").append('\n');
         responseText.fillWithContents(builder);
+        builder.append("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" +
+                "▓▓▓▓▓▓▓").append('\n');
         System.out.println(new String(builder));
         builder.delete(0, builder.length());
     }
